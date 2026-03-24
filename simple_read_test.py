4e23b135f0576d4fee2f7c028cf3246d89d1c0c1
lorithai <lorithai@gmail.com>
2026-03-24 11:30:14 +0100

    parsing

--- simple_read_test.py
+++ simple_read_test.py
@@ -4,7 +4,12 @@ import os
 import shutil
 from pathlib import Path
 
+import json
+import xml.etree.ElementTree as ET
+
+#%%
 base_dir = os.path.dirname(os.path.abspath(__file__))
+VALID_EXT = {".json", ".xml", ".rels", ".model", ".config"}
 
 #%% open and extract 3mf file to a folder
 input_file = os.path.join(base_dir, "Baby_Dragon_Multicolor_FF_P1P_X1C.3mf")
@@ -31,34 +36,29 @@ def build_tree(path: Path):
                 "path": str(item),
                 "parsed": None  # will hold parsed content later
             })
-    
+
     return node
 
-def print_tree(tree, indent=0):
-    for name, value in tree.items():
-        print("  " * indent + name)
-        if isinstance(value, dict):
-            print_tree(value, indent + 1)
-
 tree = build_tree(Path(output_path))
-print_tree(tree)
 
+#%% parse files based on extension
 
+filepath = tree["children"][0]["children"][0]["path"]
+print("filepath:", filepath)
 #%%
-for root, dirs, files in os.walk(output_path, topdown=True):
-    print("dirs:", dirs, "files:", files)
-    for name in files:
-        rel_path = os.path.relpath(os.path.join(root, name), output_path)
-        #print(rel_path)
-        #if any(rel_path.startswith(prefix) for prefix in REMOVE_PREFIXES):
-        #    os.remove(os.path.join(root, name))
+def parse_file(path: Path):
+    try:
+        if path.suffix == ".json":
+            return json.loads(path.read_text())
 
-        # Repack
-        #with zipfile.ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED) as zout:
-        #    for root, dirs, files in os.walk(output_path):
-        #        for file in files:
-        #            full_path = os.path.join(root, file)
-        #            rel_path = os.path.relpath(full_path, tmpdir)
-         #           zout.write(full_path, rel_path)
+        elif path.suffix in [".xml", ".rels", ".model", ".config"]:
+            return ET.fromstring(path.read_text())
 
-#%%
+        else:
+            return None
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+parse_file(Path(filepath))
